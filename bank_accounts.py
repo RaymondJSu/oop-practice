a1acb15fd5ed9d4d10d38f3e39c72259e63eaf56
Raymond Su <raymondsu@Raymonds-MacBook-Pro.local>
2023-10-17 16:09:40 -0700

    add deposit, withdraw function

--- bank_accounts.py
+++ bank_accounts.py
@@ -1,5 +1,33 @@
+class BalanceException(Exception):
+    pass
+
 class BankAccount:
     def __init__(self, initAmount, acctName):
         self.balance = initAmount
         self.name = acctName
-        print(f"\nAccount '{self.name}' created.\nBalance = ${self.balance}:.2f")
+        print(f"\nAccount '{self.name}' created.\nBalance = ${self.balance:.2f}")
+
+    def getBalance(self):
+        print(f"\nAccount '{self.name}' Balance = ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"\n Deposite complete.")
+        self.getBalance()
+
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return 
+        else:
+            raise BalanceException(
+                f"\nSorry, account '{self.name}' only has a balance of ${self.balance:.2f}"
+            )
+
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\nWithdraw complete.")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\nWithdraw interupted. {error}")
